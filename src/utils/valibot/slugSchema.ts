6a1dfd415fa744318ff8f4083785df5de493207a
David Siewert <david1gruppenplan@gmail.com>
2025-10-28 12:22:02 +0600

    feat(utils): add slug schema, with tests and human readable error/description messages

---
+++ src/utils/valibot/slugSchema.ts
@@ -0,0 +1,17 @@
+import * as v from "valibot"
+import { inputMaxLength25 } from "~ui/input/input/inputMaxLength"
+
+const regexMessage1 = "must only consist of lowercase letters, digits and hyphens"
+const regexMessage2 = "no consecutive hyphens"
+const regexMessage3 = "no leading hyphens"
+const regexMessage4 = "no trailing hyphens"
+
+export const slugSchema = v.pipe(
+  v.string(),
+  v.regex(/^[a-z0-9-]+$/, regexMessage1),
+  v.regex(/^(?!.*--)/, regexMessage2),
+  v.regex(/^[^-]/, regexMessage3),
+  v.regex(/[^-]$/, regexMessage4),
+  v.minLength(3),
+  v.maxLength(inputMaxLength25),
+)
