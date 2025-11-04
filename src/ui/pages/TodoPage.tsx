3f68e6efe873a3bdce0960b05688f6dc7bf6f9d1
David Siewert <david1gruppenplan@gmail.com>
2025-11-04 11:22:02 +0600

    style(todo page): add navbar

--- src/ui/pages/TodoPage.tsx
+++ src/ui/pages/TodoPage.tsx
@@ -1,9 +1,31 @@
+import { OrganizationListLinkButton } from "@/app/nav/links/OrganizationListLinkButton"
+import { WorkspaceListLinkButton } from "@/app/nav/links/WorkspaceListLinkButton"
+import { NavBreadcrumbSeparator } from "@/app/nav/NavBreadcrumbSeparator"
+import { NavStatic } from "@/app/nav/NavStatic"
+import { LinkLikeText } from "@/ui/links/LinkLikeText"
 import { TodoSection, type TodoSectionProps } from "@/ui/pages/TodoSection"
 import { PageWrapper } from "~ui/static/page/PageWrapper"
 
 export function TodoPage(p: TodoSectionProps) {
   return (
     <PageWrapper>
+      <NavStatic
+        dense={false}
+        childrenLeft={
+          p.title && (
+            <>
+              <NavBreadcrumbSeparator />
+              <LinkLikeText>{p.title}</LinkLikeText>
+            </>
+          )
+        }
+        childrenCenter={
+          <>
+            <OrganizationListLinkButton />
+            <WorkspaceListLinkButton />
+          </>
+        }
+      />
       <TodoSection {...p} />
     </PageWrapper>
   )
