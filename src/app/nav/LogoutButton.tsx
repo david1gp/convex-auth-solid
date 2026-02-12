abfd047c7adf11dd92a215e12626f0d9b87baac8
David Siewert <david1gruppenplan@gmail.com>
2026-02-12 19:06:37 +0600

    fix(logout): remove from sessions on autologin

--- src/app/nav/LogoutButton.tsx
+++ src/app/nav/LogoutButton.tsx
@@ -1,16 +1,26 @@
-import { userSessionSignal } from "@/auth/ui/signals/userSessionSignal"
+import { autoLoginIfUserRoleOnly, userSessionSignal } from "@/auth/ui/signals/userSessionSignal"
+import { userSessionsSignal } from "@/auth/ui/signals/userSessionsSignal"
 import { ttt } from "~ui/i18n/ttt"
 import { buttonVariant } from "~ui/interactive/button/buttonCva"
 import { ButtonIcon } from "~ui/interactive/button/ButtonIcon"
 import { linkIcons } from "~ui/static/icon/linkIcons"
+import type { MayHaveClass } from "~ui/utils/MayHaveClass"
 
-export function LogoutButton() {
+export interface LogoutButtonProps extends MayHaveClass {
+  text?: string
+}
+
+export function LogoutButton(p: LogoutButtonProps) {
   function onClick() {
     userSessionSignal.set(null)
+    const autoLogin = autoLoginIfUserRoleOnly()
+    if (autoLogin) {
+      userSessionsSignal.set([])
+    }
   }
   return (
-    <ButtonIcon icon={linkIcons.exit} onClick={onClick} variant={buttonVariant.link}>
-      {ttt("Logout")}
+    <ButtonIcon icon={linkIcons.exit} onClick={onClick} variant={buttonVariant.link} class={p.class}>
+      {p.text ?? ttt("Logout")}
     </ButtonIcon>
   )
 }
