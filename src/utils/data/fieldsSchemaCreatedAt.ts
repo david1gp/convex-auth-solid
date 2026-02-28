9cb16acd6a38473d336e686bb446b061244200bd
David Siewert <david1gruppenplan@gmail.com>
2026-02-28 12:41:49 +0600

    refactor(utils): remove unused utility files

--- src/utils/data/fieldsSchemaCreatedAt.ts
+++
@@ -1,8 +0,0 @@
-import * as a from "valibot"
-import { dateTimeSchema } from "~utils/valibot/dateTimeSchema"
-
-export const fieldsSchemaCreatedAt = {
-  createdAt: dateTimeSchema,
-} as const
-
-export type FieldsSchemaCreatedAtType = typeof fieldsSchemaCreatedAt
