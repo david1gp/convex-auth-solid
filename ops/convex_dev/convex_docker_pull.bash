19a2834f6024336aacb8612b13d2f235a29f9334
David Siewert <david1gruppenplan@gmail.com>
2026-03-31 16:09:09 +0600

    ops(convex): fix pull script

--- ops/convex_dev/convex_docker_pull.bash
+++ ops/convex_dev/convex_docker_pull.bash
@@ -2,4 +2,5 @@
 set -x # Print all executed commands to the terminal
 set -e # Exit immediately if a command exits with a non-zero status
 
-docker pull ghcr.io/get-convex/convex-backend:latest ghcr.io/get-convex/convex-dashboard:latest
+docker pull ghcr.io/get-convex/convex-backend:latest
+docker pull ghcr.io/get-convex/convex-dashboard:latest
